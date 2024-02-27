12f7162935984cb79c4bf0ddf581cbba0949cb2e
CLCK0622 <zhongyi070622@163.com>
2024-02-27 10:26:37 +0800

    0227-shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -17,9 +21,12 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
   }
 
-public static final int Pegion2_ID = 0;
-public static final int LFa = 2, LFv = 3, LFe = 1, LF0 = 1444;
-public static final int RFa = 11, RFv = 10, RFe = 4, RF0 = 107;
-public static final int RBa = 12, RBv = 8, RBe = 7, RB0 = 3907;
-public static final int LBa = 6, LBv = 5, LBe = 9, LB0 = 2180;
+  public static final int Pegion2_ID = 0;
+  public static final int LFa = 2, LFv = 3, LFe = 1, LF0 = 1444;
+  public static final int RFa = 11, RFv = 10, RFe = 4, RF0 = 107;
+  public static final int RBa = 12, RBv = 8, RBe = 7, RB0 = 3907;
+  public static final int LBa = 6, LBv = 5, LBe = 9, LB0 = 2180;
+
+  public static final int ShooterLMoterID = 30;
+  public static final int ShooterRMoterID = 29;
 }
